ab87a4e7c1d86aea03642addb4077fc3998f4f98
WebMechanic <serradeil@webmechanic.biz>
2012-08-26 19:28:52 +0200

    11. Formulare cleverer

---
+++ jforms.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin mit Beispielen für JForm Felder- ohne wirkliche Funktion und
+ * im praktischen Einsatz damit recht wertfrei. Die XML-Datei enthält
+ * eine Auswahl der Feldtypen aus dem JForm-Paket der J!Platform.
+ *
+ * @author   WebMechanic.biz
+ * @version  0.1.0
+ * @license  CC-BY-NC 3.0/de
+ */
+
+// FB::log(__FILE__);
